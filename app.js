f780b166c3936532f3f6a5b625350dc17b7437a0
Sanil <sanil@Sanils-MacBook-Air.local>
2022-05-22 17:20:01 +0530

    Add gitignore,profile and update port

--- app.js
+++ app.js
@@ -170,7 +170,12 @@ app.get('/about', function(req,res)
 res.render("about")
 });
 
-app.listen(3000,function()
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port,function()
 {
   console.log("server started on port 3000");
 });
